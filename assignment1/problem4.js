ee8c8a50504da89c0f567ba67edb6d2ba82fb7d7
Nitish <nitishchhimpa410@gmail.com>
2023-01-03 22:03:34 +0530

    first commit

---
+++ assignment1/problem4.js
@@ -0,0 +1,13 @@
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+let name="lavish"
+let school="J.N.V School"
+let grade="12th"
+let section="A"
+let rollno=1206
+let marks="HINDI = 80 ENGLISH = 93 MATHS = 89"
+console.log("name-", name)
+console.log("school-" ,school)
+console.log("grade-", grade)
+console.log("section-", section)
+console.log("rollno-", rollno)
+console.log("marks-", marks)
